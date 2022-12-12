2af9e5a1fb0300aa75a1827b97625e0f8e811303
alec-moore <34519622+alec-moore@users.noreply.github.com>
2022-12-12 14:05:09 -0800

    Add files via upload

--- 09/index_p1.ts
+++ 09/index_p1.ts
@@ -14,16 +14,24 @@ async function main() {
         move(trackingMap, positions, instruction.move, instruction.count);
     }
 
-    console.log(countMap(trackingMap));
+    console.log(countMap(trackingMap), positions);
 
     return;
 }
 
 function countMap(trackingMap){
     let count = 0;
-    for(let y = 0; y < trackingMap.length; y++)
-        for(let x = 0; x < trackingMap[y].length; x++)
-            if(trackingMap[y][x]) count++;
+    for(let y = 0; y < trackingMap.length; y++){
+        let mappy = '';
+        for(let x = 0; x < trackingMap[y].length; x++){
+            if(trackingMap[y][x] === true){
+                count++;
+                mappy += 'x'
+            }
+            else  mappy += 'o';
+        }
+        console.log(mappy)
+    }
 
     return count;
 }
@@ -34,69 +42,71 @@ interface Positions{
 }
 
 function move(trackingMap: boolean[][], positions: Positions, direction: string, count: number): void{
-    if(count <= 0) return;
-
-    switch(direction){
-        case 'R':
-            //if tail is up or down from head, don't move
-            if(
-                positions.tail.y !== (positions.head.y + 1)
-                && positions.tail.y !== (positions.head.y - 1)
-            ){
-                // If same spot, don't move tail
-                if(positions.tail.x !== positions.head.x && positions.tail.y !== positions.head.y)
-                    positions.tail.x++;
-                positions.head.x++;
-            }
-            break;
-        case 'L':
-            //if tail is up or down from head, don't move
-            if(
-                positions.tail.y !== (positions.head.y + 1)
-                && positions.tail.y !== (positions.head.y - 1)
-            ){
-                if(positions.tail.x !== positions.head.x && positions.tail.y !== positions.head.y)
-                    positions.tail.x--;
-                positions.head.x--;
-            }
-            break;
-        case 'U':
-            //if tail is right or left from head
-            if(
-                positions.tail.x !== (positions.head.x + 1)
-                && positions.tail.x !== (positions.head.x - 1)
-            ){
-                if(positions.tail.x !== positions.head.x && positions.tail.y !== positions.head.y)
-                    positions.tail.y--;
-                positions.head.y--;
-
-            }
-            //should be inverse for array
-            break;
-        case 'D':
-            //if tail is right of left from head
-            if(
-                positions.tail.x !== (positions.head.x + 1)
-                && positions.tail.x !== (positions.head.x - 1)
-            ){
-                if(positions.tail.x !== positions.head.x && positions.tail.y !== positions.head.y)
-                    positions.tail.y++;
-                positions.head.y++;
-            }
-            break;
-    }
-
-    if(trackingMap.length <= positions.tail.y)
-        while(trackingMap.length < positions.tail.y) 
-            trackingMap.push([])
+    while(count-- > 0){
+        switch(direction){
+            case 'R':
+                //if tail is up or down from head, don't move
+                if(
+                    positions.tail.y !== (positions.head.y + 1)
+                    && positions.tail.y !== (positions.head.y - 1)
+                ){
+                    // If same spot, don't move tail
+                    if(positions.tail.x !== positions.head.x || positions.tail.y !== positions.head.y)
+                        positions.tail.x++;
+                    positions.head.x++;
+                }
+                break;
+            case 'L':
+                //if tail is up or down from head, don't move
+                if(
+                    positions.tail.y !== (positions.head.y + 1)
+                    && positions.tail.y !== (positions.head.y - 1)
+                ){
+                    if(positions.tail.x !== positions.head.x || positions.tail.y !== positions.head.y)
+                        positions.tail.x--;
+                    positions.head.x--;
+                }
+                break;
+            case 'U':
+                //if tail is right or left from head
+                if(
+                    positions.tail.x !== (positions.head.x + 1)
+                    && positions.tail.x !== (positions.head.x - 1)
+                ){
+                    if(positions.tail.x !== positions.head.x || positions.tail.y !== positions.head.y)
+                        positions.tail.y--;
+                    positions.head.y--;
     
-    if(trackingMap[positions.tail.y].length <= positions.tail.x) 
-        while(trackingMap[positions.tail.y].length <= positions.tail.x) 
-            trackingMap[positions.tail.y].push(false);
-
-    trackingMap[positions.tail.y][positions.tail.x] = true;
-    return move(trackingMap, positions, direction, count--);
-
+                }
+                //should be inverse for array
+                break;
+            case 'D':
+                //if tail is right of left from head
+                if(
+                    positions.tail.x !== (positions.head.x + 1)
+                    && positions.tail.x !== (positions.head.x - 1)
+                ){
+                    if(positions.tail.x !== positions.head.x || positions.tail.y !== positions.head.y)
+                        positions.tail.y++;
+                    positions.head.y++;
+                }
+                break;
+        }
+        if(positions.tail.y < 0) positions.tail.y = 0;
+        if(positions.tail.x < 0) positions.tail.x = 0;
+        if(positions.head.y < 0) positions.head.y = 0;
+        if(positions.head.x < 0) positions.head.x = 0;
+        
+        if(trackingMap.length <= (positions.tail.y + 1))
+            while(trackingMap.length <= (positions.tail.y + 1)) 
+                trackingMap.push([])
+        
+        if(trackingMap[positions.tail.y].length <= (positions.tail.x + 1)) 
+            while(trackingMap[positions.tail.y].length <= (positions.tail.x + 1)) 
+                trackingMap[positions.tail.y].push(false);
+    
+        trackingMap[positions.tail.y][positions.tail.x] = true;
+    }
 }
 
 async function getInstructions(input): Promise<{move: string; count: number}[]> {
